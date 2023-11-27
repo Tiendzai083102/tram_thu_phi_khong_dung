75f91c27b15b10f0e1a11772a4e9969f572be353
manh1357902 <manh7a2002@gmail.com>
2023-11-27 22:46:57 +0700

    update

--- tramthuphikhongdung/TramThuPhiKhongDung/src/main/java/com/example/tramthuphikhongdung/entity/Goi.java
+++ tramthuphikhongdung/TramThuPhiKhongDung/src/main/java/com/example/tramthuphikhongdung/entity/Goi.java
@@ -9,14 +9,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Goi {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,4 +25,41 @@ public class Goi {
     private User user;
     @ManyToOne(targetEntity = LoaiGoi.class)
     private LoaiGoi loaiGoi;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getBatdau() {
+		return batdau;
+	}
+	public void setBatdau(Date batdau) {
+		this.batdau = batdau;
+	}
+	public Date getKetthuc() {
+		return ketthuc;
+	}
+	public void setKetthuc(Date ketthuc) {
+		this.ketthuc = ketthuc;
+	}
+	public boolean isTrangthai() {
+		return trangthai;
+	}
+	public void setTrangthai(boolean trangthai) {
+		this.trangthai = trangthai;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public LoaiGoi getLoaiGoi() {
+		return loaiGoi;
+	}
+	public void setLoaiGoi(LoaiGoi loaiGoi) {
+		this.loaiGoi = loaiGoi;
+	}
+    
 }
--- tramthuphikhongdung/TramThuPhiKhongDung/src/main/java/com/example/tramthuphikhongdung/entity/LoaiGoi.java
+++ tramthuphikhongdung/TramThuPhiKhongDung/src/main/java/com/example/tramthuphikhongdung/entity/LoaiGoi.java
@@ -1,14 +1,12 @@
 package com.example.tramthuphikhongdung.entity;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class LoaiGoi {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +17,29 @@ public class LoaiGoi {
     private String loaixe;
     @Column(nullable = false)
     private Integer gia;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLoaive() {
+		return loaive;
+	}
+	public void setLoaive(String loaive) {
+		this.loaive = loaive;
+	}
+	public String getLoaixe() {
+		return loaixe;
+	}
+	public void setLoaixe(String loaixe) {
+		this.loaixe = loaixe;
+	}
+	public Integer getGia() {
+		return gia;
+	}
+	public void setGia(Integer gia) {
+		this.gia = gia;
+	}
+    
 }
